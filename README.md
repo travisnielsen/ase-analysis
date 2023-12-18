857837b5f05e77984ccb983d6e22ccfc482d9c27
Travis Nielsen <trniel@microsoft.com>
2023-12-18 15:44:14 -0600

    add ase and tags

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# App Service Environment Analysis Scripts
+# App Service Environment Information Export
 
-TBD
+This script exports information about Azure [App Service Enviroment](https://learn.microsoft.com/en-us/azure/app-service/environment/overview) (ASE) deployments across all subscriptions within a given tenant. This is useful for planning upgrades to ASEv3 as part of the [upcoming retirement of ASEv2 in August 2024](https://azure.microsoft.com/en-us/updates/app-service-environment-version-1-and-version-2-will-be-retired-on-31-august-2024-2/). Data will only be provided for subscriptions the user has Read access to.
+
+To generate the CSV files, use the following command:
+
+```powershell
+.\aseinfo.ps1 -tenantId "YOUR_TENANT_ID" -orgPrefix "YOUR_PREFIX"
+```
+
+The `-orgPrefix` can be any value. Its used as a prefix for the output files.
--- scripts/aseinfo.ps1
+++ scripts/aseinfo.ps1
@@ -15,6 +15,34 @@ $appServicePlans = Get-AzResource -ResourceType "Microsoft.Web/serverfarms" -Exp
 
 foreach ($subscription in $subscriptions) {
     Set-AzContext -Subscription $subscription.Id -Tenant $tenantId
+
+
+    $appServiceEnvironmentsV2 = Get-AzResource -ResourceType "Microsoft.Web/hostingEnvironments" -ExpandProperties | Where-Object { $_.Kind -eq "ASEV2" }
+
+    foreach ($ase in $appServiceEnvironmentsV2) {
+        Write-Information "Processing $($ase.Name)"
+        $subscriptionProperties = @{ SubscriptionName = $subscription.Name }
+        $serviceProperties = @{ 
+            ResourceGroup = $ase.Properties.resourceGroup;
+            DnsSuffix = $ase.Properties.dnsSuffix;
+            VnetName = $ase.Properties.vnetName;
+            VnetSubnetName = $ase.Properties.vnetSubnetName;
+            VnetResourceGroupName = $ase.Properties.vnetResourceGroupName;
+            VirtualNetworkId = $ase.Properties.virtualNetwork.id;
+            Status = $ase.Properties.status;
+            UpgradeAvailability = $ase.Properties.upgradeAvailability;
+            InternalLoadBalancingMode = $ase.Properties.internalLoadBalancingMode;
+            ProvisioningState = $ase.Properties.provisioningState;
+            Tags = $ase.Tags | ConvertTo-Json -Compress;
+        }
+
+        $aseInfo = $ase | Select-Object -Property ResourceName, Kind, ResourceGroupName, Location, Id, SubscriptionId
+        $aseInfo | Add-Member $subscriptionProperties
+        $aseInfo | Add-Member $serviceProperties
+        $aseInfo | Export-Csv -Path "${orgPrefix}-appserviceenvironmentinfo.csv" -Append -NoTypeInformation -UseQuotes AsNeeded
+    }
+
+
     $appServicePlans = Get-AzResource -ResourceType "Microsoft.Web/serverfarms" -ExpandProperties | Where-Object { $_.Sku.Tier -eq "Isolated" }
 
     foreach ($plan in $appServicePlans) {
@@ -26,13 +54,14 @@ foreach ($subscription in $subscriptions) {
             Status = $plan.Properties.status; 
             MaximumNumberOfWorkers = $plan.Properties.maximumNumberOfWorkers; 
             Reserved = $plan.Properties.reserved;
-            Kind = $plan.Properties.kind
+            Kind = $plan.Properties.kind;
+            Tags = $plan.Tags | ConvertTo-Json -Compress;
         }
         $appServicePlanInfo = $plan | Select-Object -Property Name, Id, ResourceGroupName, Location
         $appServicePlanInfo | Add-Member $skuProperties
         $appServicePlanInfo | Add-Member $subscriptionProperties
         $appServicePlanInfo | Add-Member $serviceProperties
-        $appServicePlanInfo | Export-Csv -Path "${orgPrefix}-appserviceplaninfo.csv" -Append -NoTypeInformation
+        $appServicePlanInfo | Export-Csv -Path "${orgPrefix}-appserviceplaninfo.csv" -Append -NoTypeInformation -UseQuotes AsNeeded
 
         $appSvcPlan = Get-AzAppServicePlan -Name $plan.Name -ResourceGroupName $plan.ResourceGroupName
 
@@ -47,10 +76,11 @@ foreach ($subscription in $subscriptions) {
                 NumberOfWorkers = $webApp.SiteConfig.NumberOfWorkers;
                 WindowsFxVersion = $webApp.SiteConfig.WindowsFxVersion;
                 LinuxFxVersion = $webApp.SiteConfig.LinuxFxVersion;
+                Tags = $webApp.Tags | ConvertTo-Json -Compress;
                 }
             $webAppInfo = $webapp | Select-Object -Property Name, State, Id, Kind, DefaultHostName, ResourceGroup
             $webAppInfo | Add-Member $subProperties
-            $webAppInfo | Export-Csv -Path "${orgPrefix}-webappinfo.csv" -Append -NoTypeInformation
+            $webAppInfo | Export-Csv -Path "${orgPrefix}-webappinfo.csv" -Append -NoTypeInformation -UseQuotes AsNeeded
         }
     }
 
